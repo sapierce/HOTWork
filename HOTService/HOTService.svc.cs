48ad4a0051d9d30a15da89bee3856b8167688417
stephanie <lowlysacker@gmail.com>
2015-08-08 14:39:53 -0500

    Addition of Service

---
+++ HOTService/HOTService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using HOTBAL;
+
+namespace HOTService
+{
+    public class HOTService : IHOTService
+    {
+        SDAMethods method = new SDAMethods();
+        public List<Art> GetAllArts()
+        {
+            return method.GetAllSDAArts();
+        }
+    }
+}
